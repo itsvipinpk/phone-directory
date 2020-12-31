84c415708497c77c581d3a5a9bf195bab9d613f2
Vipin P K <51770037+itsvipinpk@users.noreply.github.com>
2020-12-31 19:31:40 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ class App extends Component {
   render() {
     return (
       <div>
+        {/* <div>
+          Phone Directory !
+              </div>
 
+        <div>
+          <span>Name</span><br />
+          <span>Phone</span>
+        </div> */}
+
+        <label htmlFor='name'>Name</label>
+        <input id='name' type='text' placeholder='Type Here..'></input>
       </div>
     );
   }
-  }
+}
 
-  export default App;
+export default App;
