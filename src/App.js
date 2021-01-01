8d76817f48a56182b170cb542318cbfd623ceedd
Vipin P K <51770037+itsvipinpk@users.noreply.github.com>
2021-01-01 13:17:45 +0530

    Styled ‘Header’ (External Styling)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component }  from 'react';
 import Header from "./Header";
 
+import "./Header.css"
+
 class App extends Component {
   render() {
     return (
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,20 @@
 import React  from 'react';
 
+import "./Header.css"
+
 //this is functional component !
 const Header = function(){
 
+    const headerStyle = {textAlign:'center', padding:20, background:'#000', color:'#fff', textTransform:'uppercase'};
+
     return(
 
-        <div>
-          Phone Directory !
+        // <div style={headerStyle}>
+        //   Phone Directory !
+        // </div>
+
+         <div className="header">
+        Phone Directory !
         </div>
 
     )
